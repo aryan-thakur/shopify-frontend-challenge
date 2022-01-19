acba3752ba8ba69c2f95e7756beaa7458476902d
Aryan Thakur <aryanthakur@Aryan.local>
2022-01-19 00:46:22 -0500

    comments added

--- app/AngularJS/Controllers/mainController.js
+++ app/AngularJS/Controllers/mainController.js
@@ -1,9 +1,13 @@
 app.controller('mainController', function($scope, $http, $route, imageService, apikey) {
 $scope.loader = false;
+
+//initialize the application with the following date
 $scope.init = () => {
   $scope.search = new Date(2022,0,1);
   $scope.triggerCall({});
 }
+
+//function to take UTC month and return a month number ranging 1-12 (as a string)
 var makeMonth = (month) => {
   month = month+1;
   if(month < 10){
@@ -14,9 +18,14 @@ var makeMonth = (month) => {
   }
 }
 $scope.photos = [];
+
+//main function to trigger the API call
 $scope.triggerCall = (e) => {
+  //create a correctly formatted date for the API
   const earthdate = $scope.search.getFullYear().toString()+'-'+makeMonth($scope.search.getUTCMonth())+'-'+$scope.search.getUTCDate().toString();
+  //start loading screen
   $scope.loader = true;
+  //on response, populate appropriate array if successful. Whether successful or not, stop loading screen once API responds.
   imageService.getjsonData(earthdate, apikey.key).then((data) => {$scope.loader= false; $scope.photos = data.data.photos;}, ()=>{$scope.loader=false; console.log("error!")});
 }
 })
--- app/AngularJS/Services/imageService.js
+++ app/AngularJS/Services/imageService.js
@@ -1,14 +1,10 @@
 app.factory('imageService', ['$http', function($http) {
-  var apiObj = {};
-  var apiEndpointBase = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?";
+  let apiObj = {};
+  let apiEndpointBase = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?";
   apiObj.getjsonData = (earthdate, key) => {
   let apiEndpoint = apiEndpointBase + 'earth_date=' + earthdate;
   apiEndpoint = apiEndpoint + "&api_key=" + key
   return $http.get(apiEndpoint, {withCredentials: false})
-  .then(
-  (result) => {return result;},
-  () => {return {};}
-  );
-  };
+  .then((result) => {return result;},() => {return {};});};
   return apiObj;
 }]);
